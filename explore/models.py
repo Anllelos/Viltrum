f8410620449c2345d1e8f9678fde34051601ab0e
Adrian <adrian@viltrum.com>
2024-10-15 21:17:23 -0600

    Implementacion de la app explore

--- explore/models.py
+++ explore/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class Game(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.CharField(max_length=100)  # Ensure to store categories like Action, Shooter, etc.
-    popularity = models.IntegerField()  # For sorting by popularity
-    release_date = models.DateField()  # For sorting by release date
-    cover_image = models.ImageField(upload_to='game_covers/')  # For displaying game covers
+    category = models.CharField(max_length=50)
+    image = models.ImageField(upload_to='games/')
+    is_live = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
--- explore/urls.py
+++ explore/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.explore_home, name='explore'),  # Main explore page
+    path('explore/', views.explore, name='explore'),
 ]
+
