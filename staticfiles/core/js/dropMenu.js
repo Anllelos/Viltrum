61bd04a03353ec780ca34c8f30220aceedda9587
Adrian <adrian@viltrum.com>
2024-10-15 21:25:01 -0600

    Implementacion de la app explore 2

--- staticfiles/core/js/dropMenu.js
+++ staticfiles/core/js/dropMenu.js
@@ -1,19 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var userIcon = document.getElementById("userIcon");
-    var dropdownMenu = document.getElementById("dropdownMenu");
+  const userIcon = document.getElementById("userIcon");
+  const dropdownMenu = document.getElementById("dropdownMenu");
+  const notifyIcon = document.getElementById("notifyIcon");
+  const dropdownMenuNotify = document.getElementById("dropdownMenuNotify");
 
-    userIcon.addEventListener("click", function (event) {
+  userIcon.addEventListener("click", function (event) {
       event.preventDefault();
-      dropdownMenu.style.display =
-        dropdownMenu.style.display === "block" ? "none" : "block";
-    });
-
-    document.addEventListener("click", function (event) {
-      if (
-        !userIcon.contains(event.target) &&
-        !dropdownMenu.contains(event.target)
-      ) {
-        dropdownMenu.style.display = "none";
-      }
-    });
+      const isOpen = dropdownMenu.classList.toggle("show");
+      userIcon.setAttribute("aria-expanded", isOpen);
+      // Evita que el evento de clic en el documento cierre el dropdown inmediatamente
+      event.stopPropagation();
   });
+
+  notifyIcon.addEventListener("click", function (event) {
+      event.preventDefault();
+      const isOpen = dropdownMenuNotify.classList.toggle("show");
+      notifyIcon.setAttribute("aria-expanded", isOpen);
+      // Evita que el evento de clic en el documento cierre el dropdown inmediatamente
+      event.stopPropagation();
+  });
+
+  document.addEventListener("click", function (event) {
+      // Cierra los dropdowns si se hace clic fuera de ellos
+      if (!userIcon.contains(event.target) && !dropdownMenu.contains(event.target)) {
+          dropdownMenu.classList.remove("show");
+          userIcon.setAttribute("aria-expanded", "false");
+      }
+
+      if (!notifyIcon.contains(event.target) && !dropdownMenuNotify.contains(event.target)) {
+          dropdownMenuNotify.classList.remove("show");
+          notifyIcon.setAttribute("aria-expanded", "false");
+      }
+  });
+});
