0895deab9bf07f90ef4bef578143d0b4e6d2f52c
ADRI3L18 <adrian.robelo@outlook.com>
2024-11-19 10:36:08 -0600

    back end de filtros clasificaciones

--- clasifications/models.py
+++ clasifications/models.py
@@ -4,17 +4,10 @@ from django.contrib.auth.models import User
 class PlayerStats(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='clasifications_playerstats')
     game = models.CharField(max_length=100)
-    viltrum_rank = models.CharField(max_length=10, choices=[
-        ('E', 'Elite'),
-        ('S', 'Sakura'),
-        ('S+', 'Sakura +'),
-        ('V', 'Viltrum'),
-        ('V+', 'Viltrum +')
-    ])
     wins = models.IntegerField()
     losses = models.IntegerField()
     total_played = models.IntegerField()  # Hours played
     user_game = models.CharField(max_length=100)  # Game-specific username
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username} - {self.game}"
